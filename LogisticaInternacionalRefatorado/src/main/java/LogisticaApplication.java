ac2409be38640e660208a4673d049ba75877c103
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-13 16:24:00 -0300

    feat: Adiciona main

--- LogisticaInternacionalRefatorado/src/main/java/LogisticaApplication.java
+++ LogisticaInternacionalRefatorado/src/main/java/LogisticaApplication.java
@@ -1,6 +1,35 @@
+import domain.strategy.FreteRepository;
+import domain.strategy.FreteStrategy;
+import domain.strategy.NotificacaoService;
+import infra.notification.EmailService;
+import infra.persistence.SqlServerRepository;
+import infra.transport.FreteAereoStrategy;
+import infra.transport.FreteMaritimoStrategy;
+import service.LogisticaService;
+
+import java.util.Scanner;
 
 public class LogisticaApplication {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        FreteRepository freteRepository = new SqlServerRepository();
+        NotificacaoService notificacaoService = new EmailService();
+        LogisticaService service = new LogisticaService(freteRepository,notificacaoService);
+
+        System.out.println("--- INTERGLOBAL LOGISTICS ---");
+        System.out.print("Peso da carga: ");
+        double peso = scanner.nextDouble();
+
+        System.out.println("Escolha o modal: 1- Marítimo | 2- Aéreo");
+        int opcao = scanner.nextInt();
+
+        FreteStrategy estrategiaSelecionada = (opcao == 1)
+                ? new FreteMaritimoStrategy()
+                : new FreteAereoStrategy();
+
+        service.calcularEnvio(peso, estrategiaSelecionada);
 
     }
 }
--- LogisticaInternacionalRefatorado/src/main/java/infra/transport/FreteAereoStrategy.java
+++ LogisticaInternacionalRefatorado/src/main/java/infra/transport/FreteAereoStrategy.java
@@ -2,7 +2,7 @@ package infra.transport;
 
 import domain.strategy.FreteStrategy;
 
-public class FreteAeroStrategy implements FreteStrategy {
+public class FreteAereoStrategy implements FreteStrategy {
     @Override
     public double calcularFrete(double peso) {return (peso * 15.0) + 500; }
 }
