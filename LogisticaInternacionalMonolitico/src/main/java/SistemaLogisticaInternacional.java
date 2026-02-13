a9ceb98c291d674cc67a3d323d889c3d0e5f1a2e
amanda_salvador <amanda_salvador@estudante.sesisenai.org.br>
2026-02-13 15:38:37 -0300

    feat: implementa entrada de dados do usuario

--- LogisticaInternacionalMonolitico/src/main/java/SistemaLogisticaInternacional.java
+++ LogisticaInternacionalMonolitico/src/main/java/SistemaLogisticaInternacional.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class SistemaLogisticaInternacional {
 
     public void processarFrete(){
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("--- SISTEMA DE LOGÍSTICA (MONOLÍTICO) ---");
+        System.out.println("Digite o peso da carga (kg): ");
+        double peso = scanner.nextDouble();
+
+        System.out.println("Digite o tipo de transporte (1- Maritimo, 2- Aereo): ");
+        int tipo = scanner.nextInt();
     }
 
     public static void main(String[]args){
