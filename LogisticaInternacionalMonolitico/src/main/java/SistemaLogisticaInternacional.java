cb532273a04ae7c2333313745c161195a2cb32b1
amanda_salvador <amanda_salvador@estudante.sesisenai.org.br>
2026-02-13 15:34:42 -0300

    feat: adiciona estrutura principal do sistema

--- LogisticaInternacionalMonolitico/src/main/java/SistemaLogisticaInternacional.java
+++ LogisticaInternacionalMonolitico/src/main/java/SistemaLogisticaInternacional.java
@@ -1,2 +1,11 @@
 public class SistemaLogisticaInternacional {
+
+    public void processarFrete(){
+
+    }
+
+    public static void main(String[]args){
+        SistemaLogisticaInternacional sistema = new SistemaLogisticaInternacional();
+        sistema.processarFrete();
+    }
 }
