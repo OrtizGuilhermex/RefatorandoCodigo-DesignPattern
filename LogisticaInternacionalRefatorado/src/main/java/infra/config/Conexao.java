570098e249a4b1357bc4b68bb33d8626207e9ae5
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-16 19:37:55 -0300

    feat: Adiciona conexao com bd via JDBC

---
+++ LogisticaInternacionalRefatorado/src/main/java/infra/config/Conexao.java
@@ -0,0 +1,16 @@
+package infra.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    private static String URL = "jdbc:mysql://localhost:3306/logistica?useSSL=false&serverTimezone=UTC";
+    private static String USER = "root";
+    private static String PASSWORD = "";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
+}
--- LogisticaInternacionalRefatorado/src/main/java/infra/persistence/SqlServerRepository.java
+++ LogisticaInternacionalRefatorado/src/main/java/infra/persistence/SqlServerRepository.java
@@ -2,8 +2,30 @@ package infra.persistence;
 
 import domain.model.Frete;
 import domain.strategy.FreteRepository;
+import infra.config.Conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class SqlServerRepository implements FreteRepository {
+
     @Override
-    public void salvar(Frete frete) {{ System.out.println("Salvando no SQL Server..."); }}
+    public void salvar(Frete frete) {
+        String sql = "INSERT INTO fretes (peso, valor_base, imposto, total) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setDouble(1, frete.getPeso());
+            stmt.setDouble(2, frete.getValorFrete());
+            stmt.setDouble(3, frete.getImposto());
+            stmt.setDouble(4, frete.getValorFrete() + frete.getImposto());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.err.println("[ERRO DB] Falha ao persistir dados: " + e.getMessage());
+        }
+    }
 }
